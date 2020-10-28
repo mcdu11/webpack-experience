ba254515348341d651a5a094f53da8d8a40ccb2d
dwq <duwenqang@sobey.com>
2020-10-28 19:57:31 +0800

    clean dist in prod

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// 做一些清理
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 // 此插件为每个包含 CSS 的 JS 文件创建一个单独的 CSS 文件，并支持 CSS 和 SourceMap 的按需加载。
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require("webpack");
@@ -57,7 +55,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "Output Management",
       template: "index.html",
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,13 @@
 const { merge } = require("webpack-merge");
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+// 做一些清理
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   devtool: "source-map",
-//   plugins: [new UglifyJSPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    // new UglifyJSPlugin(),
+  ],
 });
