78ac0a1858627b6835999f0528649edd04bc7e0d
dwq <duwenqang@sobey.com>
2020-10-28 17:12:59 +0800

    using HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
 
 const distPath = path.resolve(__dirname, "dist");
 
 module.exports = {
   entry: {
     app: "./src/index.js",
-    print: "./src/print.js",
   },
   devtool: "inline-source-map",
   devServer: {
     contentBase: "./dist",
     port: 8081,
+    hot: true,
   },
   output: {
     filename: "[name].bundle.js",
@@ -24,5 +25,9 @@ module.exports = {
       title: "Output Management",
       template: "index.html",
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
+  optimization: {
+    moduleIds: 'named'
+  }
 };
